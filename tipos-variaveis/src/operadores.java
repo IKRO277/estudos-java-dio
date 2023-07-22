20896992a0c944c8ca8000a56fb97e86d1ef80d8
Icaro Pereira De Paula <136732875+IKRO277@users.noreply.github.com>
2023-07-22 18:56:38 -0300

    operadores

--- tipos-variaveis/src/operadores.java
+++ tipos-variaveis/src/operadores.java
@@ -15,6 +15,37 @@ public class operadores {
             else
                 resultado = "Falso";
         System.out.println(resultado);
+        // ou posso usar(String resultado = a==b ? "Verdadeiro" : "Falso";)
+        int numero1, numero2;
+        numero1 = 4;
+        numero2 = 6;
 
+        boolean simNao = numero1 == numero2; 
+
+            System.out.println("numero1 é igual numero2 ? " + simNao);
+
+        simNao = numero1 != numero2;
+
+            System.out.println("numero1 é diferente de numero2 ? " + simNao);
+
+        boolean condicao1 = true;
+        boolean condicao2 = false;
+
+        if(condicao1 && condicao2){
+            System.out.println("As duas são verdadeiras");
+        }
+        if(condicao1 || condicao2){
+            System.out.println("Pelo menos uma é verdadeira");
+        }
+        System.out.println("fim");
+
+        int number1 = (5+4);
+        int number5 = (5+9);
+
+        if(number1 == number5){
+        System.out.println("é igualzin");
+        }
+        else
+        System.out.println("não é igualzin");
     }
 }
