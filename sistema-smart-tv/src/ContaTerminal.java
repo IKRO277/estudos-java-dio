6a6b330cb2ecafe55f7720dd65f76568ebc9a4a0
Icaro Pereira De Paula <136732875+IKRO277@users.noreply.github.com>
2023-07-24 10:32:14 -0300

    mais uma aí

--- sistema-smart-tv/src/ContaTerminal.java
+++ sistema-smart-tv/src/ContaTerminal.java
@@ -6,14 +6,14 @@ public class ContaTerminal {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        System.out.println("Digite seu nome inteiro:");
+        System.out.println("Digite seu nome:");
         String nome = scanner.next();
 
         System.out.println("Digite seu email:");
         String email = scanner.next();
 
         System.out.println("Digite seu número de telefone:");
-        int numero = scanner.nextInt();
+        String numero = scanner.next();
 
         System.out.println("Digite sua idade:");
         int idade = scanner.nextInt();
@@ -21,11 +21,20 @@ public class ContaTerminal {
         System.out.println("Seu nome inteiro é " + nome);
         System.out.println("Seu email é " + email);
         System.out.println("Seu número é " + numero);
-        System.out.println("E sua idade é de " + idade + "anos");
+        System.out.println("E sua idade é de " + idade + " anos");
 
         System.out.println("Todos esses dados estão corretos? caso sim, digite 1, caso não, digite 0.");
-        int afirmação = scanner.nextInt();
-        if
+        int afirmaçao = scanner.nextInt();
+
+        if(afirmaçao == 1){
+              System.out.println("Cadastro realizado.");
+        }
+        if(afirmaçao == 0){
+              System.out.println("Por favor realize novamente o seu cadastro.");
+        }
+        
+
+     
         
     }
 
