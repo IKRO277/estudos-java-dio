ce3b33d1d06422e4d89b3739ebf2123e9643f1b8
Icaro Pereira De Paula <136732875+IKRO277@users.noreply.github.com>
2023-08-13 11:15:26 -0300

    sim

---
+++ AnaliseCandidatos/controle-candidatos/src/candidatura/Contador.java
@@ -0,0 +1,39 @@
+package candidatura;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = scanner.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = scanner.nextInt();
+        
+        try {
+            // Chamando o método contendo a lógica de contagem
+            contar(parametroUm, parametroDois);
+        
+        } catch (ParametrosInvalidosException exception) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for (int base = 0; base < contagem; base++) {
+            System.out.println("numero " + base);
+        }
+    }
+}
+
+class ParametrosInvalidosException extends RuntimeException {
+    public ParametrosInvalidosException() {
+        super("Parâmetros inválidos: o segundo parâmetro deve ser maior que o primeiro");
+    }
+}
